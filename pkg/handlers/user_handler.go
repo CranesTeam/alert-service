abc875eeedd78b3479d548b12a0db1a9d5f9cb58
ilya <ilya.selfrealization@gmail.com>
2023-06-27 23:54:12 +0200

    [kw-001] init and added peace of code

---
+++ pkg/handlers/user_handler.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func New(server *http.ServeMux) http.Handler {
+	// OauthGoogle
+	server.HandleFunc("/auth/google/login", oauthGoogleLogin)
+	server.HandleFunc("/auth/google/callback", oauthGoogleCallback)
+	return server
+}
+
+func oauthGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	// Create oauthState cookie
+	oauthState := generateStateOauthCookie(w)
+
+	/*
+		AuthCodeURL receive state that is a token to protect the user from CSRF attacks.
+		You must always provide a non-empty string and
+		validate that it matches the the state query parameter on your redirect callback.
+	*/
+	u := googleOauthConfig.AuthCodeURL(oauthState)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(20 * time.Minute)
+
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "" // todo: fix it
+	}
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
+}
+
+func getUserDataFromGoogle(code string) ([]byte, error) {
+	// Use code to get token and get user info from Google.
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
+	}
+
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+
+	return contents, nil
+}
+
+func oauthGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	// Read oauthState from Cookie
+	oauthState, _ := r.Cookie("oauthstate")
+
+	if r.FormValue("state") != oauthState.Value {
+		log.Println("invalid oauth google state")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getUserDataFromGoogle(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// TODO: GetOrCreate User in your db.
+	// Redirect or response with a token.
+	fmt.Fprintf(w, "UserInfo: %s\n", data)
+}
